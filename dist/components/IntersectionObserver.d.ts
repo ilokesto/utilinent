51720551a5d780167972fc151e95e9157c974506
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-06-27 19:09:20 +0900

    feat: IntersectionObserver 및 관련 파일 삭제, 패키지 이름 수정

--- dist/components/IntersectionObserver.d.ts
+++
@@ -1,2 +0,0 @@
-import type { IntersectionObserverProps } from "./types";
-export declare function IntersectionObserver({ children, fallback, threshold, rootMargin, triggerOnce, onIntersect, }: IntersectionObserverProps): import("react/jsx-runtime").JSX.Element;
