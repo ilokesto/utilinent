0e23fc9632f697f2601014fd95ae90121bebf082
Jinho Ayden Jeong <144667387+ayden94@users.noreply.github.com>
2025-09-08 15:16:06 +0900

    feat: Mount 컴포넌트를 추가하고 index 파일에 내보내기 수정

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export * from './components/For';
-export * from './components/Mount';
 export * from './components/Observer';
 export * from './components/OptionalWrapper';
 export * from './components/Show';
 
+export * from './experimental/Mount';
 export * from './experimental/Repeat';
 export * from './experimental/Slacker';
 export * from './experimental/Switch';
