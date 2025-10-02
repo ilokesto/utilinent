cd5c8222dbe039cbf77b666a9c330d55274b972b
Jinho Ayden Jeong <144667387+ayden94@users.noreply.github.com>
2025-10-02 09:43:45 +0900

    feat: For 및 Show 컴포넌트에 태그 렌더링 기능 추가 및 타입 정의 개선

--- dist/components/For.d.ts
+++ dist/components/For.d.ts
@@ -1,2 +1,9 @@
+import { ComponentPropsWithRef } from "react";
 import type { ForProps } from "../types";
-export declare function For<T extends Array<unknown>>({ each, children, fallback, }: ForProps<T>): string | number | boolean | import("react").ReactElement<any, string | import("react").JSXElementConstructor<any>> | Iterable<import("react").ReactNode> | null;
+type ForType = {
+    <T extends Array<unknown>>(props: ForProps<T>): React.ReactNode;
+} & {
+    [K in keyof JSX.IntrinsicElements]: <T extends Array<unknown>>(props: ForProps<T> & ComponentPropsWithRef<K>) => React.ReactNode;
+};
+export declare const For: ForType;
+export {};
--- dist/components/Show.d.ts
+++ dist/components/Show.d.ts
@@ -1,3 +1,13 @@
-import type { ShowProps, ShowPropsArray } from "../types";
-export declare function Show<T extends unknown[]>({ when, children, fallback }: ShowPropsArray<T>): React.ReactNode;
-export declare function Show<T extends unknown>({ when, children, fallback }: ShowProps<T>): React.ReactNode;
+import { ComponentPropsWithRef } from "react";
+import { ShowProps, ShowPropsArray } from "../types";
+type ShowType = {
+    <T extends unknown[]>(props: ShowPropsArray<T>): React.ReactNode;
+    <T extends unknown>(props: ShowProps<T>): React.ReactNode;
+} & {
+    [K in keyof JSX.IntrinsicElements]: {
+        <T extends unknown[]>(props: ShowPropsArray<T> & ComponentPropsWithRef<K>): React.ReactNode;
+        <T extends unknown>(props: ShowProps<T> & ComponentPropsWithRef<K>): React.ReactNode;
+    };
+};
+export declare const Show: ShowType;
+export {};
