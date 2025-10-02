0d1078c85e8713a87d5275f0d484108f3f41dd61
Jinho Ayden Jeong <144667387+ayden94@users.noreply.github.com>
2025-10-02 09:42:54 +0900

    feat: For 및 Show 컴포넌트에 태그 렌더링 기능 추가 및 타입 정의 개선

--- src/components/For.tsx
+++ src/components/For.tsx
@@ -1,11 +1,40 @@
+import { ComponentPropsWithRef, createElement } from "react";
 import type { ForProps } from "../types";
 
-export function For<T extends Array<unknown>>({
+type ForType = {
+  <T extends Array<unknown>>(props: ForProps<T>): React.ReactNode;
+} & {
+  [K in keyof JSX.IntrinsicElements]: <T extends Array<unknown>>(
+    props: ForProps<T> & ComponentPropsWithRef<K>
+  ) => React.ReactNode;
+};
+
+function BaseFor<T extends Array<unknown>>({
   each,
   children,
   fallback = null,
 }: ForProps<T>) {
-  return (each && each.length > 0)
-    ? each?.map(children)
-    : fallback;
+  return each && each.length > 0 ? each.map(children) : fallback;
 }
+
+const renderForTag =
+  (tag: any) =>
+  <T extends Array<unknown>>({
+    each,
+    children,
+    fallback = null,
+    ...props
+  }: ForProps<T> & ComponentPropsWithRef<any>) => {
+    if (!each || each.length === 0) return fallback;
+    const content = each.map(children);
+    return createElement(tag, props, content);
+  };
+
+const htmlTags = [
+  "a","abbr","address","article","aside","b","bdi","bdo","blockquote","button","canvas","cite","code","data","datalist","dd","del","details","dfn","dialog","div","dl","dt","em","fieldset","figcaption","figure","footer","form","h1","h2","h3","h4","h5","h6","header","hr","i","img","input","ins","kbd","label","legend","li","main","map","mark","menu","meter","nav","ol","option","output","p","picture","pre","progress","q","rp","rt","ruby","s","samp","section","select","small","span","strong","sub","summary","sup","table","tbody","td","textarea","tfoot","th","thead","time","tr","u","ul","var","video"
+];
+
+export const For = Object.assign(
+  BaseFor,
+  Object.fromEntries(htmlTags.map(tag => [tag, renderForTag(tag)]))
+) as unknown as ForType;
--- src/components/Show.tsx
+++ src/components/Show.tsx
@@ -1,8 +1,17 @@
-import type { ShowProps, ShowPropsArray } from "../types";
+import { ComponentPropsWithRef, createElement } from "react";
+import { ShowProps, ShowPropsArray } from "../types";
 
-export function Show<T extends unknown[]>({ when, children, fallback }: ShowPropsArray<T>): React.ReactNode;
-export function Show<T extends unknown>({ when, children, fallback }: ShowProps<T>): React.ReactNode;
-export function Show<T,>({ when, children, fallback = null }: any): React.ReactNode {
+type ShowType = {
+  <T extends unknown[]>(props: ShowPropsArray<T>): React.ReactNode;
+  <T extends unknown>(props: ShowProps<T>): React.ReactNode;
+} & {
+  [K in keyof JSX.IntrinsicElements]: {
+    <T extends unknown[]>(props: ShowPropsArray<T> & ComponentPropsWithRef<K>): React.ReactNode;
+    <T extends unknown>(props: ShowProps<T> & ComponentPropsWithRef<K>): React.ReactNode;
+  }
+};
+
+const BaseShow = <T,>({ when, children, fallback = null }: ShowProps<T> | ShowPropsArray<T[]>) => {
   const shouldRender = Array.isArray(when) ? when.every(Boolean) : !!when;
 
   return shouldRender
@@ -11,3 +20,22 @@ export function Show<T,>({ when, children, fallback = null }: any): React.ReactN
       : children
     : fallback;
 };
+
+const renderForTag =
+  (tag: any) =>
+  ({ when, children, fallback = null, ...props }: (ShowProps<any> | ShowPropsArray<any[]>) & ComponentPropsWithRef<any>) => {
+    const shouldRender = Array.isArray(when) ? when.every(Boolean) : !!when;
+    if (!shouldRender) return fallback;
+
+    const content = typeof children === "function" ? children(when as any) : children;
+    return createElement(tag, props, content);
+  };
+
+const htmlTags = [
+  "a","abbr","address","article","aside","b","bdi","bdo","blockquote","button","canvas","cite","code","data","datalist","dd","del","details","dfn","dialog","div","dl","dt","em","fieldset","figcaption","figure","footer","form","h1","h2","h3","h4","h5","h6","header","hr","i","img","input","ins","kbd","label","legend","li","main","map","mark","menu","meter","nav","ol","option","output","p","picture","pre","progress","q","rp","rt","ruby","s","samp","section","select","small","span","strong","sub","summary","sup","table","tbody","td","textarea","tfoot","th","thead","time","tr","u","ul","var","video"
+];
+
+export const Show = Object.assign(
+  BaseShow,
+  Object.fromEntries(htmlTags.map(tag => [tag, renderForTag(tag)]))
+) as unknown as ShowType;
