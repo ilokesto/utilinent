4b62dbb7584c3267e42f638e9b6f6349290d2dd0
Jinho Ayden Jeong <144667387+ayden94@users.noreply.github.com>
2025-10-02 11:44:20 +0900

    feat: useIntersectionObserver 훅을 index.d.ts 및 index.js에 추가 refactor: RepeatType 인터페이스 및 관련 타입 정의 추가

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,4 @@ export * from './components/Observer';
 export * from './components/OptionalWrapper';
 export * from './components/Repeat';
 export * from './components/Show';
+export * from './hooks/useIntersectionObserver';
